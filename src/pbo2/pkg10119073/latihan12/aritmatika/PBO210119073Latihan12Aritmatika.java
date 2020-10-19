0e05441850b8e3f69a5d04a77f50c67135ce54cc
aghniadm <aghniadewii@gmail.com>
2020-10-19 08:59:42 +0700

    PBO2-10119073-Latihan12-Aritmatika

---
+++ src/pbo2/pkg10119073/latihan12/aritmatika/PBO210119073Latihan12Aritmatika.java
@@ -0,0 +1,32 @@
+package pbo2.pkg10119073.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * aritmatika
+ *
+ */
+public class PBO210119073Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
